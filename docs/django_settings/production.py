f274130ce071e2371b216ce3846db5beaa8ed405
Artem Zenkov <azenkov@isgneuro.com>
2021-05-21 14:12:46 +0700

    generate django secret key in file with first launch

--- docs/django_settings/production.py
+++ docs/django_settings/production.py
@@ -2,8 +2,17 @@
 production settings
 """
 import os
+
+from django.core.management.utils import get_random_secret_key
 from .base import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ.get('SECRET_KEY', None)
+# put secret key in secret_key file for the first launch
+secret_key_file = BASE_DIR / 'secret_key'
+
+if secret_key_file.exists():
+    SECRET_KEY = secret_key_file.read_text()
+else:
+    SECRET_KEY = get_random_secret_key()
+    secret_key_file.write_text(SECRET_KEY)
