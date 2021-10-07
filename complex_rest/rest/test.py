b7a5aeaf187e23828506346169b2b214b7e8bb25
Artem Zenkov <azenkov@isgneuro.com>
2021-10-08 00:02:23 +0700

    allow all databases for testing

--- complex_rest/rest/test.py
+++ complex_rest/rest/test.py
@@ -1,2 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase as DjangoTestCase
 from rest_framework.test import APIClient
+
+class TestCase(DjangoTestCase):
+    databases = '__all__'
+
