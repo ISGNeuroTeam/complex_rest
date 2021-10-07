a791f344efd6f918b1f85b1c98e03d704688a5db
Artem Zenkov <azenkov@isgneuro.com>
2021-10-07 23:57:13 +0700

    plugin test databases for testing

--- complex_rest/core/settings/test.py
+++ complex_rest/core/settings/test.py
@@ -2,6 +2,7 @@
 django settings for testing
 """
 from .base import *
+
 DEBUG = False
 
 DATABASES = {
@@ -14,6 +15,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'auth_db.sqlite3',
     },
 }
+
+# change database engine for testing to sqlite for plugins databases
+for database_name, database_config in plugin_databases.items():
+    database_config['ENGINE'] = 'django.db.backends.sqlite3'
+    database_config['NAME'] = BASE_DIR / f'{database_name}.sqlite3'
+
+DATABASES.update(plugin_databases)
+
 
 INSTALLED_APPS.remove('plugin_example1')
 INSTALLED_APPS.remove('plugin_example2')
