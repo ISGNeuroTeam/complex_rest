71386dabe32bdb30af0cc89a1f4f3e9930421595
Artem Zenkov <azenkov@isgneuro.com>
2021-11-01 12:12:39 +0700

    success response and error response

--- complex_rest/rest/response.py
+++ complex_rest/rest/response.py
@@ -1,3 +1,26 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import APIException
+
+
+class SuccessResponse(Response):
+    def __init__(self, data=None, http_status=status.HTTP_200_OK):
+        data = data or {}
+        data.update({
+            'status': 'success',
+            'status_code': http_status,
+            'error': ''
+        })
+        super().__init__(data, http_status)
+
+
+class ErrorResponse(Response):
+    def __init__(self, data=None, http_status=status.HTTP_400_BAD_REQUEST, error_message=''):
+        data = data or {}
+        data.update({
+            'status': 'error',
+            'status_code': http_status,
+            'error': error_message
+        })
+        super().__init__(data, http_status)
+
