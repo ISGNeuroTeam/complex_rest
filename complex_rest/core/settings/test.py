e2afa3ad94ae49789fb638cba40865a031186e61
Artem Zenkov <azenkov@isgneuro.com>
2021-11-26 12:20:53 +0700

    test database in files (without 'TEST:NAME' setting the database will be created in memory, )

--- complex_rest/core/settings/test.py
+++ complex_rest/core/settings/test.py
@@ -5,24 +5,36 @@ from .base import *
 
 DEBUG = False
 
+default_database_name = str(BASE_DIR / 'default_db.sqlite3')
+auth_database_name = str(BASE_DIR / 'auth_db.sqlite3')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'default_db.sqlite3',
+        'NAME': default_database_name,
+        'TEST': {
+            'NAME': default_database_name,
+        },
     },
     'auth_db': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'auth_db.sqlite3',
+        'NAME': auth_database_name,
+        'TEST': {
+            'NAME': auth_database_name,
+        },
     },
 }
 
 # change database engine for testing to sqlite for plugins databases
 for database_name, database_config in plugin_databases.items():
+    plugin_database_name = str(BASE_DIR / f'{database_name}.sqlite3')
+
     database_config['ENGINE'] = 'django.db.backends.sqlite3'
-    database_config['NAME'] = BASE_DIR / f'{database_name}.sqlite3'
+    database_config['NAME'] = plugin_database_name
+    database_config['TEST'] = {
+        'NAME': plugin_database_name,
+    }
 
 DATABASES.update(plugin_databases)
-
 
 INSTALLED_APPS.remove('plugin_example1')
 INSTALLED_APPS.remove('plugin_example2')
