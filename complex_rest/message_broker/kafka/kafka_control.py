d944e3191081ead1f037ea229ce486cadaa4ec7c
Artem Zenkov <azenkov@isgneuro.com>
2022-02-14 21:13:46 +0700

    fix GroupCoordinatorNotAvailableError raise when first launch

--- complex_rest/message_broker/kafka/kafka_control.py
+++ complex_rest/message_broker/kafka/kafka_control.py
@@ -1,6 +1,6 @@
 from kafka import KafkaAdminClient
 from kafka.admin.new_partitions import NewPartitions
-from kafka.errors import InvalidPartitionsError, KafkaError
+from kafka.errors import InvalidPartitionsError, KafkaError, GroupCoordinatorNotAvailableError
 from kafka.admin.new_topic import NewTopic
 
 
@@ -24,22 +24,26 @@ class KafkaControl:
         topic_info = self.client.describe_topics([topic, ])[0]
         partitions_count = len(topic_info['partitions'])
 
-        consumer_group_info = self.client.describe_consumer_groups([consumer_group, ])[0]
+        try:
+            consumer_group_info = self.client.describe_consumer_groups([consumer_group, ])[0]
 
-        # find topic consumers in consumer group
-        topic_consumers = [
-            member for member in consumer_group_info.members
-            if member.member_assignment and len(member.member_assignment.assignment) > 0 and\
-            member.member_assignment.assignment[0][0] == topic
-        ]
+            # find topic consumers in consumer group
+            topic_consumers = [
+                member for member in consumer_group_info.members
+                if member.member_assignment and len(member.member_assignment.assignment) > 0 and\
+                member.member_assignment.assignment[0][0] == topic
+            ]
 
-        # how many consumers at the moment
-        consumers_count = len(topic_consumers)
+            # how many consumers at the moment
+            consumers_count = len(topic_consumers)
 
+            # create one more partition for consumer
+            if consumers_count != 0 and consumers_count >= partitions_count:
+                self.create_partitions(topic, consumers_count + 1)
 
-        # create one more partition for consumer
-        if consumers_count != 0 and consumers_count >= partitions_count:
-            self.create_partitions(topic, consumers_count + 1)
+        except GroupCoordinatorNotAvailableError as err:
+            # this error occurs when first consumer is being created because group coordinator doesn't exist
+            pass
 
     def create_topic_if_not_exist(self, topic, partitions):
         rsp = self.client.describe_topics([topic, ])
