adbc17446e86b0546119f5967f17f45399a09597
egor-lukyanov <elukianov@isgneuro.com>
2022-05-12 14:16:34 +0300

    ref: get back

--- complex_rest/rest_auth/authorization.py
+++ complex_rest/rest_auth/authorization.py
@@ -1,9 +1,7 @@
 from typing import Optional, Any, Callable
-from rest.response import status
-from django.http import HttpResponse
-from core.settings.base import PLUGINS_DIR
-from .exceptions import AccessDeniedError, OwnerIDError, KeyChainIDError
-from .models import User, KeyChain, Action
+
+from .exceptions import AccessDeniedError, OwnerIDError, KeyChainIDError, ActionError
+from .models import User, KeyChain, Action, Role, Permit
 
 
 class BaseProtectedResource:
@@ -22,11 +20,6 @@ class BaseProtectedResource:
         self.owner_id = owner_id
         self.keychain_id = keychain_id
 
-    @staticmethod
-    def get_plugin_name(view_filepath: str):
-        plugin = view_filepath[view_filepath.find(PLUGINS_DIR) + len(PLUGINS_DIR) + 1:]
-        return plugin[:plugin.find('/')]
-
 
 def check_authorization(action: str, when_denied: Optional[Any] = None, on_error: Optional[Callable] = None):
     def deco(func):
@@ -37,22 +30,37 @@ def check_authorization(action: str, when_denied: Optional[Any] = None, on_error
                 if on_error:
                     return on_error(f"Owner with ID {obj.owner_id} unknown")
                 raise OwnerIDError("Owner unknown", obj.owner_id)
+
             try:
-                keychain = KeyChain.objects.get(id=obj.keychain_id)
-            except KeyChain.DoesNotExist:
+                act = Action.objects.get(name=action, plugin__name=obj.plugin)
+            except Action.DoesNotExist:
                 if on_error:
-                    return on_error(f"Keychain with ID {obj.owner_id} unknown")
-                raise KeyChainIDError("keychain unknown", obj.keychain_id)
+                    return on_error(f"Action with name {action} unknown")
+                raise ActionError("Action unknown", action)
 
-            act = Action.objects.get(name=action, plugin__name=obj.plugin)  # what if action Doesn't exist?
-            is_owner = obj.user == owner
+            is_owner = obj.user == owner if owner else None
 
-            permissions = {
-                permit.allows(
-                    obj.user, act, by_owner=is_owner) for permit in keychain.permissions if permit.affects_on(obj.user)
-            }
+            if obj.keychain_id:
+                try:
+                    permits = KeyChain.objects.get(id=obj.keychain_id).permissions
+                except KeyChain.DoesNotExist:
+                    if on_error:
+                        return on_error(f"Keychain with ID {obj.owner_id} unknown")
+                    raise KeyChainIDError("keychain unknown", obj.keychain_id)
+            else:
+                permits = Permit.objects.filter(
+                    actions=act,
+                    roles__in=Role.objects.filter(groups__in=obj.user.groups.all())
+                )
 
-            if permissions and permissions != {None}:  # what if {None, None}
+            permissions = (
+                {
+                    permit.allows(
+                        obj.user, act, by_owner=is_owner) for permit in permits if permit.affects_on(obj.user)
+                }
+            )
+
+            if permissions and permissions != {None}:
                 if False not in permissions:
                     return func(obj, *args, **kwargs)
             else:
