1456ed0bea5feb2c72ef40a13c01577a2f0899c3
Artem Zenkov <azenkov@isgneuro.com>
2022-06-23 16:44:45 +0700

    add drf spectacular descriptions in auth

--- complex_rest/rest_auth/views.py
+++ complex_rest/rest_auth/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest.views import APIView
 from rest.response import Response
 
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiResponse
+
 from . import serializers
 from .authentication import AUTH_HEADER_TYPES
 from .exceptions import InvalidToken, TokenError
@@ -29,6 +31,46 @@ class Login(generics.GenericAPIView):
             self.www_authenticate_realm,
         )
 
+    @extend_schema(
+        summary='login',
+        request=serializers.AccessTokenSerializer,
+        responses=serializers.TokenResponseSerializer,
+        description="""
+        Takes user login and password, returns user access token
+        Sets cookie 'auth_token' with value 'Bearer <token>'
+        """,
+        examples=[
+            OpenApiExample(
+                name='Login request',
+                value={
+                    "login": "admin",
+                    "password": "admin"
+                },
+                request_only=True,
+   
+            ),
+            OpenApiExample(
+                name="Success response",
+                value={
+                    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU2MDE1NjQ3LCJpYXQiOjE2NTU5NzI0NDcsImp0aSI6ImNkNTlmMDgxYzM5YzQ5OTBhNGU3NWU1NzM2NzYxNDQ0IiwiZ3VpZCI6IjMxNjk5OWFmLWU3NTctNDE2ZC1hZjQ2LTliNDJhNDY5ZjYwNiIsImxvZ2luIjoiYWRtaW4ifQ.xyJrmq7dSCnV3pnFFKUjj_jf4Md1rCb9pcFIq-61ZWE",
+                    "status": "success",
+                    "status_code": 200,
+                    "error": ""
+                },
+                response_only=True
+            ),
+            OpenApiExample(
+                name='Login failed',
+                value={
+                    "detail": "No active account found with the given credentials",
+                    "status_code": 401,
+                    "status": "error",
+                    "error": "Unauthorized"
+                },
+                response_only=True
+            )
+        ]
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
@@ -44,15 +86,38 @@ class Login(generics.GenericAPIView):
 
 class Logout(APIView):
     permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.LogoutSerializer
 
+    @extend_schema(
+        summary='logout',
+        description="logout by removing http only cookie",
+        responses={
+            200: OpenApiResponse(
+                response=serializers.LogoutResponseSerializer,
+                description='User logged out'
+            )
+
+        },
+        examples=[
+            OpenApiExample(
+                name='Success response',
+                value={
+                    "message": "admin logged out",
+                    "status": "success",
+                    "status_code": 200,
+                    "error": ""
+                },
+                response_only=True
+            ),
+        ]
+    )
     def delete(self, request):
         """logout by removing http only cookie"""
         current_user = request.user.username
-        response = Response(
+        response = SuccessResponse(
             {
                 'message': f'{current_user} logged out',
-            },
-            status.HTTP_200_OK
+            }
         )
         response.delete_cookie('auth_token')
         request.session.flush()  # clear session
