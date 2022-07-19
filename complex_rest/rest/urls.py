d6fe726bb6f75eb98529b2910a743babbf9489e0
Artem Zenkov <azenkov@isgneuro.com>
2022-07-19 16:19:09 +0700

    add arguments to build_django_path

--- complex_rest/rest/urls.py
+++ complex_rest/rest/urls.py
@@ -1,10 +1,15 @@
-import typing
+from typing import Union
 
-from django.urls import path as _django_path
+from django.urls import path as _django_path, re_path as _django_re_path
 
 
-def build_django_path(url: str, handler: typing.Callable):
-    return _django_path(url, handler)
+def build_django_path(route: str, view: Union[list, tuple, callable], kwargs=None, name=None):
+    return _django_path(route, view, kwargs=kwargs, name=name)
+
+
+def build_django_re_path(route: str, view: Union[list, tuple, callable], kwargs=None, name=None):
+    return _django_re_path(route, view, kwargs=kwargs, name=name)
 
 
 path = build_django_path
+re_path = build_django_re_path
