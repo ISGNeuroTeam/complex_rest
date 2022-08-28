9ac339c69afdb0e162058056c18d9a64b7b1ff32
Artem Zenkov <azenkov@isgneuro.com>
2022-08-28 15:10:21 +0700

    rename base_auth_covered

--- complex_rest/rest_auth/models/base_auth_covered.py
+++ complex_rest/rest_auth/models/base_auth_covered.py
@@ -1,17 +1,19 @@
 import logging
 
+from abc import ABCMeta
+
 from django.db import models
-from mixins.models import NamedModel
 from rest_auth.authentication import User
-from rest_auth.abc import IAuthCovered
 from mixins.models import NamedModel, TimeStampedModel
-from rest_auth.models import KeyChainModel
+
+from .containers import KeyChainModel
+from .abc import IAuthCovered
 
 
 log = logging.getLogger('root.rest_auth')
 
 
-class BaseAuthCovered(IAuthCovered, NamedModel, TimeStampedModel):
+class BaseAuthCoveredModel(IAuthCovered, NamedModel, TimeStampedModel):
     object_id = models.CharField(max_length=256, blank=True, null=False, unique=True)
     _owner_id = models.PositiveIntegerField()
     _keychain_id = models.PositiveIntegerField()
--- complex_rest/rest_auth/models/containers.py
+++ complex_rest/rest_auth/models/containers.py
@@ -1,13 +1,13 @@
 import logging
 
-
 from django.db import models
 from django.core.validators import int_list_validator
 from mptt.models import MPTTModel, TreeForeignKey
 from mixins.models import NamedModel, TimeStampedModel
 from rest_auth.authentication import User
-from rest_auth.models import Permit, Action
-from rest_auth.abc import IAuthCovered, IKeyChain
+
+from .permissions import Permit, Action
+from .abc import IKeyChain
 
 
 log = logging.getLogger('root.rest_auth')
@@ -25,7 +25,9 @@ class SecurityZone(NamedModel, TimeStampedModel, MPTTModel):
 class KeyChainModel(IKeyChain, TimeStampedModel):
     keychain_id = models.CharField('keychain_id', unique=True, max_length=255)
     _zone = models.IntegerField(null=True, blank=True)
-    _permits = models.CharField(validators=int_list_validator(sep=','))
+    _permits = models.CharField(
+        max_length=1024, validators=[int_list_validator(sep=','), ]
+    )
 
     @property
     def zone(self) -> SecurityZone:
@@ -41,9 +43,6 @@ class KeyChainModel(IKeyChain, TimeStampedModel):
     def __str__(self):
         return f'{self.keychain_id}'
 
-    class Meta:
-        unique_together = ('plugin', 'keychain_id',)
-
     @property
     def permissions(self):
         return self.permits.all().union(self.zone.effective_permissions if self.zone else Permit.objects.none())
@@ -52,3 +51,6 @@ class KeyChainModel(IKeyChain, TimeStampedModel):
         return any([
             permit.allows(user, act, by_owner) for permit in self.permissions
         ])
+
+    class Meta:
+        abstract = True
