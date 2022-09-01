58fbdc534e9a9b0c91cfc9906c15645289522765
Artem Zenkov <azenkov@isgneuro.com>
2022-09-01 16:10:46 +0700

    add AuthCovered class in test plugin

--- tests/plugins/rolemodel_test/models.py
+++ tests/plugins/rolemodel_test/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
+from rest_auth.authorization import auth_covered_method
+from rest_auth.models import AuthCoveredModel
+
 from rest_auth.models import KeyChainModel, AuthCoveredModel
 
 
@@ -10,3 +13,15 @@ class PluginKeychain(KeyChainModel):
 
 class SomePluginAuthCoveredModel(AuthCoveredModel):
     keychain_model = PluginKeychain
+
+    @auth_covered_method(action_name='test.create')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @auth_covered_method(action_name='test.protected_action1')
+    def test_method1(self):
+        print('calling test auth covered method1')
+
+    
+
+
