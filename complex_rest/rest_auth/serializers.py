6e82d227175ebb0639ae244d547913a71289b358
Artem Zenkov <azenkov@isgneuro.com>
2022-12-09 09:46:03 +0700

    hash user password

--- complex_rest/rest_auth/serializers.py
+++ complex_rest/rest_auth/serializers.py
@@ -1,8 +1,10 @@
 import importlib
 
 from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import update_last_login
 from django.utils.translation import gettext_lazy as _
+
 from rest_framework import exceptions, serializers
 
 from rest.serializers import ResponseSerializer
@@ -77,6 +79,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
 
 
 class TokenResponseSerializer(ResponseSerializer):
